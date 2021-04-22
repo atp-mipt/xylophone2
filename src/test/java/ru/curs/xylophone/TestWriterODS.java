52888443a369dba3a59316c6b9be276c9deb8c79
Nikita <kononenko.n@phystech.edu>
2021-04-22 14:08:39 +0300

    save before fetch PR

--- src/test/java/ru/curs/xylophone/TestWriterODS.java
+++ src/test/java/ru/curs/xylophone/TestWriterODS.java
@@ -13,17 +13,18 @@ public class TestWriterODS {
     @Test
     public void WriterODS() throws XylophoneError, FileNotFoundException {
         InputStream descrStream = TestReader.class
-                .getResourceAsStream("testdescriptor.xml");
+                .getResourceAsStream("testsaxdescriptor3.json");
         InputStream dataStream = TestReader.class
                 .getResourceAsStream("testdata.xml");
         InputStream templateStream = TestReader.class
-                .getResourceAsStream("template.ods");
+                .getResourceAsStream("template.xls");
 
         File resultFile = new File("result.ods");
         OutputStream resultStream = new FileOutputStream(resultFile);
 
         XML2Spreadsheet.process(dataStream, descrStream, templateStream,
-                OutputType.ODS, false, resultStream);
+                OutputType.ODS, true, false, resultStream);
+
         fail();
     }
 
